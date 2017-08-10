2ae50a269fea6957786bf0e7bef19ada2c0945a2
George Zacharia <george.zcharia@gmail.com>
2017-08-10 23:53:08 +0530

    build all
    documentation gives many methods including a config file.seems error in any file breaks execution

--- tutorials/execute_notebook_make_htmls.bat
+++ tutorials/execute_notebook_make_htmls.bat
@@ -1,6 +1,5 @@
-
 :: I could not figure out how to make a batch script loop over *.ipynb files and run jupyter commands
 :: I should probably just make a python script to do it
 
-jupyter nbconvert --execute this_notebook.ipynb
+jupyter nbconvert --execute *.ipynb
 
